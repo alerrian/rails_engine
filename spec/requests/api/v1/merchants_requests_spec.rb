e6a96879ea1f0156d6b96e544340f0cc5e505392
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-17 17:25:49 -0600

    Add destroy action and tests for merchant

--- spec/requests/api/v1/merchants_requests_spec.rb
+++ spec/requests/api/v1/merchants_requests_spec.rb
@@ -48,4 +48,16 @@ describe 'Merchants API' do
     expect(merchant.name).to_not eq(previous_name)
     expect(merchant.name).to eq('New')
   end
+
+  it 'sends a merchants DESTROY' do
+    merchant = create(:merchant)
+
+    expect(Merchant.count).to eq(1)
+
+    delete "/api/v1/merchants/#{merchant.id}"
+
+    expect(response).to be_successful
+    expect(Merchant.count).to eq(0)
+    expect { Merchant.find(merchant.id) }.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
