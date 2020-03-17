f7f56696b1e4d09d4949cf29978b7676258e4fe4
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-17 15:06:13 -0600

    Add test and function for create customers

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -6,4 +6,13 @@ class Api::V1::CustomersController < ApplicationController
   def show
     render json: CustomerSerializer.new(Customer.find(params[:id]))
   end
+
+  def create
+    render json: CustomerSerializer.new(Customer.create(customer_params))
+  end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:first_name, :last_name)
+    end
 end
--- spec/requests/api/v1/customer_request_spec.rb
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -23,4 +23,16 @@ describe 'CUSTOMERS API' do
     expect(response).to be_successful
     expect(customer[:data][:id]).to eq(id)
   end
+
+  it 'CREATES a new CUSTOMER' do
+    customer_params = { first_name: Faker::Name.first_name, last_name: Faker::Name.last_name }
+
+    post '/api/v1/customers', params: { customer: customer_params }
+
+    customer = Customer.last
+
+    expect(response).to be_successful
+    expect(customer.first_name).to eq(customer_params[:first_name])
+    expect(customer.last_name).to eq(customer_params[:last_name])
+  end
 end
