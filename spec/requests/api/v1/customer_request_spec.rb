68863156f642828248f13886b40c9e893fb519ef
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-17 14:25:36 -0600

    Add tests and function for customer show

--- spec/requests/api/v1/customer_request_spec.rb
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'CUSTOMERS API' do
-  it 'sends a list of CUSTOMERS' do
+  it 'sends an INDEX of CUSTOMERS' do
     create_list(:customer, 3)
 
     get '/api/v1/customers'
@@ -11,5 +11,16 @@ describe 'CUSTOMERS API' do
     customers = JSON.parse(response.body)
 
     expect(customers['data'].count).to eq(3)
+  end
+
+  it 'sends a SINGLE CUSTOMER' do
+    id = create(:customer).id
+
+    get "/api/v1/customers/#{id}"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(customer['data']['id']).to eq(id.to_s)
   end
 end
