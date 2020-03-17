71e543e58818d34940827015c3a28be0ebb392da
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-17 14:00:02 -0600

    Create initial index spec for customers

--- spec/requests/api/v1/customer_request_spec.rb
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'CUSTOMERS API' do
   it 'sends a list of CUSTOMERS' do
-    create_list(:customers, 3)
+    create_list(:customer, 3)
 
     get '/api/v1/customers'
 
