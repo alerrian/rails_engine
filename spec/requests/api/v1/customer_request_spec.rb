c09c242aa50846af289d3e6ef48457b586346e2f
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-17 15:12:44 -0600

    Add customer update test

--- spec/requests/api/v1/customer_request_spec.rb
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe 'CUSTOMERS API' do
-  it 'sends an INDEX of CUSTOMERS' do
+describe 'Customers API' do
+  it 'sends an customer INDEX' do
     create_list(:customer, 3)
 
     get '/api/v1/customers'
@@ -13,7 +13,7 @@ describe 'CUSTOMERS API' do
     expect(customers[:data].count).to eq(3)
   end
 
-  it 'sends a SINGLE CUSTOMER' do
+  it 'sends a customer SHOW' do
     id = create(:customer).id.to_s
 
     get "/api/v1/customers/#{id}"
@@ -24,7 +24,7 @@ describe 'CUSTOMERS API' do
     expect(customer[:data][:id]).to eq(id)
   end
 
-  it 'CREATES a new CUSTOMER' do
+  it 'can CREATE a customer' do
     customer_params = { first_name: Faker::Name.first_name, last_name: Faker::Name.last_name }
 
     post '/api/v1/customers', params: { customer: customer_params }
@@ -34,5 +34,19 @@ describe 'CUSTOMERS API' do
     expect(response).to be_successful
     expect(customer.first_name).to eq(customer_params[:first_name])
     expect(customer.last_name).to eq(customer_params[:last_name])
+  end
+
+  it 'can UPDATE a customer' do
+    id = create(:customer).id.to_s
+
+    previous_first_name = Customer.last.first_name
+    customer_params = { first_name: 'New' }
+
+    put "/api/v1/customers/#{id}", params: { customer: customer_params }
+    customer = Customer.find_by(id: id)
+
+    expect(response).to be_successful
+    expect(customer.first_name).to_not eq(previous_first_name)
+    expect(customer.first_name).to eq('New')
   end
 end
