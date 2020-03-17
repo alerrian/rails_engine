0a433b55811609ac9450be34fa17af85a6144a9d
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-17 13:52:47 -0600

    Add test for customer index API

---
+++ spec/requests/api/v1/customer_request_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe 'CUSTOMERS API' do
+  it 'sends a list of CUSTOMERS' do
+    create_list(:customers, 3)
+
+    get '/api/v1/customers'
+
+    expect(response).to be_successful
+  end
+end
