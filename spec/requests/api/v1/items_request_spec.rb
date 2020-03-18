b300388935bc100e24773ecca9c8c3ab1ca2c93f
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-18 15:30:05 -0600

    Add test and initial function to items index

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Items API' do
+  it 'sends a list of items' do
+    merchant_id = create(:merchant).id
+    create_list(:item, 3)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+
+    items = JSON.parse(response.body, symbolize_names: true)
+
+    expect(items.count).to eq(3)
+  end
+end
