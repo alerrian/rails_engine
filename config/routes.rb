251a09bdc0168b77ddd4a7aa06138f452bb08724
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-19 15:28:56 -0600

    Add routing and actions for mercahnt items

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers, only: [:index, :show, :create, :update, :destroy]
-      resources :merchants, only: [:index, :show, :create, :update, :destroy]
+
+      resources :merchants, only: [:index, :show, :create, :update, :destroy] do
+        scope module: :merchant do
+          resources :items, only: [:index]
+        end
+      end
 
       # scopes merchants for retrieval based on the items
       resources :items, only: [:index, :show, :create, :update, :destroy] do
