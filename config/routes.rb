442ccf176f1141c6949e739d535da84dd3211b3f
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-19 15:12:40 -0600

    Add controller action and scoped route to get item merchant

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :customers, only: [:index, :show, :create, :update, :destroy]
       resources :merchants, only: [:index, :show, :create, :update, :destroy]
-      resources :items, only: [:index, :show, :create, :update, :destroy]
+
+      # scopes merchants for retrieval based on the items
+      resources :items, only: [:index, :show, :create, :update, :destroy] do
+        scope module: :item do
+          resource :merchant, only: [:show]
+        end
+      end
     end
   end
 end
