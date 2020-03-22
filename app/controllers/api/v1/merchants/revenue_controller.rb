86efdf03f8496d747b40d6c2ac2cbd5586fa2d89
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-22 12:28:31 -0600

    Create revenue controller

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def index
-    render json: MerchantSerializer.new(Merchant.where())
+    render json: MerchantSerializer.new(Merchant.most_revenue(params[:quantity]))
   end
 end
