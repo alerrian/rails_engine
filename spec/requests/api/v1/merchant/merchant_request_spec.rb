b5f93c4c996387e20761639fdd98fe1ebdb285ee
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-22 16:40:18 -0600

    Add test to have a revenue show page for a merchant

--- spec/requests/api/v1/merchant/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant/merchant_request_spec.rb
@@ -15,4 +15,22 @@ RSpec.describe 'Item relationship endpoints' do
 
     expect(items_json[:data].length).to eq(100)
   end
+
+  it 'can send back a merchant revenue' do
+    merchant_1 = create(:merchant)
+
+    invoice_items_1 = create_list(:invoice_item, 3, quantity: 1, unit_price: 50)
+
+    invoice_items_1.each do |invoice_item|
+      invoice_item.invoice.update(merchant_id: merchant_1.id)
+      create(:transaction, invoice: invoice_item.invoice, result: 'success')
+    end
+
+    get "/api/v1/merchants/#{merchant_1.id}/revenue"
+
+    expect(response).to be_successful
+    merchant_revenue_json = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant_revenue_json[:data][:attributes][:revenue]).to eq('150.0')
+  end
 end
