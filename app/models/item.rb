f3ecc1fe74e3836af9a949452dd61eede7c3cf3b
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-03-22 08:49:53 -0600

    Add function to item find and find all end points

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,17 @@ class Item < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
+
+  def self.search_for(search_params)
+    where(search_params.keys.map { |param| "#{param} ILIKE '%#{search_params[param]}%'" }
+      .join(' AND '))
+  end
+
+  def self.get_item_by(search_params)
+    search_for(search_params).limit(1)
+  end
+
+  def self.get_all_items_by(search_params)
+    search_for(search_params)
+  end
 end
